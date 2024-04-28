89674c5f136613bf999be854d985bbebc339cc41
AMichaelOlson <anthonymolson@byui.edu>
2024-04-27 20:27:29 -0600

    Created the new code within the Program.cs file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Please enter your first and last name when prompted.");
+        Console.Write("Enter your first name: ");
+        string first = Console.ReadLine();
+
+        Console.Write("Enter your last name: ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Nice to meet you {last}, {first} {last}.");
     }
 }
