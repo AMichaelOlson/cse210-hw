359be955a9b839e4938d5c6804966820f0f54558
AMichaelOlson <anthonymolson@byui.edu>
2024-04-27 20:42:46 -0600

    Edited the Prep2 file to be congruent with the assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please type your grade percentage into the terminal: ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+
+        string grade = "";
+
+        if (percent >= 90){
+            grade = "A";
+        }
+
+        else if (percent >= 80){
+            grade = "B";
+        }
+
+        else if (percent >= 70){
+            grade = "C";
+        }
+
+        else if (percent >= 60){
+            grade = "D";
+        }
+
+        else{
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}.");
+
+        if (percent >= 70){
+            Console.WriteLine("Congrats! You passed that class!");
+        }
+
+        else{
+            Console.WriteLine("Oops, looks like you'll need to take that one again.");
+        }
     }
 }
