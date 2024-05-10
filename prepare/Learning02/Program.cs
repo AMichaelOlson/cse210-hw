cee510993dda0d9110ff979201b52883b7c716cc
AMichaelOlson <anthonymolson@byui.edu>
2024-05-10 10:25:31 -0600

    Created the files for making the resume following along with sample code/instructions from the module.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Discord";
+        job1._startYear = 2028;
+        job1._endYear = 2033;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Backend Web Dev";
+        job2._company = "Roll20";
+        job2._startYear = 2033;
+        job2._endYear = 2037;
+
+        Resume myResume = new Resume();
+        myResume._name = "Anthony Olson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
